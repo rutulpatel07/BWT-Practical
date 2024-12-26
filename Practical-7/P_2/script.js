20f5e6160aab0266692419239bd7f6c9409a3f99
Rutul Patel <rutulpatel5407@gmail.com>
2024-12-26 16:11:50 +0530

    Practical-7 Final Update

--- Practical-7/P_2/script.js
+++ Practical-7/P_2/script.js
@@ -1,3 +1,6 @@
+// Run with Node Js directly from the terminal for that you have to first install Node Js in your system. and then install coderunner extension in vs code. and then run the code by pressing ctrl+alt+n.
+
+
 var myInteger = 42;
 
 let myFloat = 3.14;
